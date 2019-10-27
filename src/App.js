cbce4f1970f66f5de7a70ade2e8255b0939187a8
Jo <Fun@MacBook-Pro-van-Jo.local>
2019-10-27 10:20:43 +0100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ class App extends Component {
   state = {
     persons: [
       {name: 'hello', age: 28},
-      {name: 'Liam', age:16}
+      {name: 'Liam', age:16},
+      {name: "Sandra", age: 37},
+
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false,
   }
 
   switchNameHandler = (newName, newAge) => {
@@ -39,26 +42,48 @@ class App extends Component {
     })
   }
 
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherir',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNameHandler("Belinda Bos", 66)}>Switch Name</button>
+        <button 
+        style={style} 
+        onClick={this.togglePersonHandler}>Toggle to change Names</button>
+
+        {
+          this.state.showPersons === true ?
+        <div>
 
         <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}/>
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
 
         <Person
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} 
         click={this.switchNameHandler.bind(this, 'Jolanda Keizer', 34)}
         changed={this.nameChangedHandler}> My hobbies: Programming </Person>
 
         <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}/>
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
 
+        </div> : null
+      }
 
       </div>
     );
